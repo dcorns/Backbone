3ee14dd8b73ab5bee704be608eeadc4cb3458f33
Dale <drcorns@hotmail.com>
2016-06-02 21:11:12 -0700

    add inheritance example

--- js/backboneModels.js
+++ js/backboneModels.js
@@ -9,7 +9,10 @@
 var Vehicle = Backbone.Model.extend({
   type: 'car',
   initialize: function(){
-    console.log('initialize runs when a new object is instantiated from the constructor');
+    console.log('Created New instance of Vehicle');
+  },
+  toString: function(){
+    return JSON.stringify(this.toJSON());
   }
 },
 //class properties come after the instance object
@@ -23,9 +26,28 @@ var Vehicle = Backbone.Model.extend({
 console.log(Vehicle.question());
 
 var ford = new Vehicle();
-var bayline = new Vehicle();
+var bayline = new Vehicle({
+  model: 'xg3',
+  length: 50
+});
 
 bayline.type = 'boat';
 
 console.log(ford.type);
 console.log(bayline.type);
+
+bayline.model = 'xg3';
+bayline.length = 50;
+console.log(bayline.toString());
+
+//bicycle inherits from vehicle
+
+var Bicycle = Vehicle.extend({});
+var schwin = new Bicycle({
+  forks: 'fixed'
+});
+
+console.log(schwin.toString());
+console.log('schwin is instance of Bicyle', schwin instanceof Bicycle);
+console.log('schwin is instance of Vehicle', schwin instanceof Vehicle);
+console.log('schwin is instance of Backbone.Model', schwin instanceof Backbone.Model);
